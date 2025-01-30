015180a0deaa67cd2f2b3a9294766d155bbdc37b
mammu <bandreddymammu599@gmail.com>
2025-01-30 09:50:58 +0530

    update catalogue1

--- variable.sh
+++ variable.sh
@@ -7,8 +7,17 @@ echo today date is ${DATE}
 ARTH=$((2-3*4/2))
 echo ARTH = ${ARTH}
 
-echo script name - $0
-echo first argument - $1
-echo second argument - $2
-echo all argument - $*
-echo no of argument - $#
+#echo script name - $0
+#echo first argument - $1
+#echo second argument - $2
+#echo all argument - $*
+#echo no of argument - $#
+
+function_name1(){
+  echo script name - $0
+  echo first argument - $1
+  echo second argument - $2
+  echo all argument - $*
+  echo no of argument - $#
+}
+function_name1 123 xyz
