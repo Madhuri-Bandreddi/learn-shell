b9cd8598cee869eac08f9f34d73612c472039cd0
mammu <bandreddymammu599@gmail.com>
2025-01-30 10:57:04 +0530

    update catalogue1

--- imdb.sh
+++ imdb.sh
@@ -4,18 +4,18 @@
 
 
 input=$1
-if [ -Z "$input" ];
+if [ -z "$input" ];
 then
   echo Input Movie Name Missing
   exit
 fi
 
 
-#percent = $(curl -s https://www.imdb.com/title/tt23849204/ratings/?ref_=tt_ov_rat)
-#echo percent - $percent
-#if [ "$percent" -ge 70 ];
-#then
- # echo Good
-#else
- # echo Average
-#fi
+percent=$(curl -s https://www.themoviedb.org/movie/$input |grep user_score_chart | xargs -n1 | grep data-percent | awk -f = "{print $2}")
+echo percent - $percent
+if [ "$percent" -ge 70 ];
+then
+  echo Good
+else
+  echo Average
+fi
