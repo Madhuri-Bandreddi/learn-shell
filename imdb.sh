b8b55c5c82acfbb21b62435bf0de03fd2c032932
mammu <bandreddymammu599@gmail.com>
2025-01-30 11:38:21 +0530

    update catalogue1

--- imdb.sh
+++ imdb.sh
@@ -15,7 +15,7 @@ percent=$(curl -s https://www.themoviedb.org/movie/$input |grep user_score_chart
 
 echo Percent - $percent
 
-if [ "$percent" -gt 70 ];
+if [ "$percent" -ge 70 ];
 then
   echo Good
 else
