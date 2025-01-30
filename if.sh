9106479965f6f86eaa3c8c503b8be72df122a42e
mammu <bandreddymammu599@gmail.com>
2025-01-30 10:10:15 +0530

    update catalogue1

--- if.sh
+++ if.sh
@@ -1,9 +1,9 @@
-fruit_name=mango
-quantity=10
-if [ $fruit_name == "mango" ]
-then
-  echo Mango Quantity - $quantity
-fi
+#fruit_name=mango
+#quantity=10
+#if [ $fruit_name == "mango" ]
+#then
+ # echo Mango Quantity - $quantity
+#fi
 
 fruit_name=$1
 quantity=$2
