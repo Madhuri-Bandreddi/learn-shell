18098fca868630f896a4d09b6f26c60bc07cfbf7
mammu <bandreddymammu599@gmail.com>
2025-01-30 10:08:50 +0530

    update catalogue1

--- if.sh
+++ if.sh
@@ -4,3 +4,12 @@ if [ $fruit_name == "mango" ]
 then
   echo Mango Quantity - $quantity
 fi
+
+fruit_name=$1
+quantity=$2
+if [ $"fruit_name" == "mango" ]
+then
+  echo Mango Quantity - $quantity
+else
+  echo fruit does not exist
+fi
