b424ea298e9bc306ae1503d4edd5fe11dbd6d382
mammu <bandreddymammu599@gmail.com>
2025-01-30 10:36:54 +0530

    update catalogue1

--- imdb.sh
+++ imdb.sh
@@ -2,12 +2,13 @@
 # > 7 - Good
 # < 7 - Average
 input=$1
-if [ - Z "$input" ] ;
+if [ -Z "$input" ];
 then
   echo Input Movie Name Missing
   exit
 fi
 percent = $(curl -s https://www.imdb.com/title/tt23849204/ratings/?ref_=tt_ov_rat)
+echo percent -$percent
 if [ "$percent" -ge 70 ];
 then
   echo Good
