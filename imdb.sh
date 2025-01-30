b86d04bbf946ca07ee6ba193009059f94a5cd7e8
mammu <bandreddymammu599@gmail.com>
2025-01-30 10:26:34 +0530

    update catalogue1

---
+++ imdb.sh
@@ -0,0 +1,16 @@
+# Get rating
+# > 7 - Good
+# < 7 - Average
+input=$1
+if [ -Z "$input" ];
+then
+  echo Input Movie Name Missing
+  exit
+fi
+percent = $(curl -s https://www.imdb.com/title/tt23849204/ratings/?ref_=tt_ov_rat)
+if [ "$percent" -ge 70 ];
+then
+  echo Good
+else
+  echo Average
+fi
