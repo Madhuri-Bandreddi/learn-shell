416196fd13202b36ed1a6d0fc7ed8e449d204b1c
mammu <bandreddymammu599@gmail.com>
2025-02-03 00:13:08 +0530

    update catalogue1

--- function.sh
+++ function.sh
@@ -16,7 +16,7 @@ function_name1() {
   echo No Of Arguments = $#
 }
 
-#a=123
+a=123
 function_name1 123 xyz
 
 # Functions will have its own exit status , but not with exit command, we have to use return command with exit states.
